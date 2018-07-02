652d910d3a174e7c08684a3094f2f276fc482736
nitinbherale <nitinbherale@gmail.com>
2018-07-02 12:05:10 +0530

    our_leader section added

---
+++ our_leader.php
@@ -0,0 +1,216 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Yuvasena Official Website</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--<link rel="apple-touch-icon" href="images/apple-touch-icon.png">-->
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="shortcut icon" href="images/favicon.Ico">
+    
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:200,300,400,400i,500,700,900" rel="stylesheet">
+    
+    <!-- Bootstrap css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    
+    <!-- Plugins css -->
+    <link rel="stylesheet" href="css/plugins.css">
+    
+    <!-- Default Css -->
+    <link rel="stylesheet" href="css/default.css">
+    
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="style.css">
+
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+<body>
+    <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+
+  <!-- Start Site Header -->
+    <header class="site-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="logo-menu-wrap hidden-xs hidden-sm">
+                        <div class="logo">
+                            <a href="index.php">
+                                <img src="images/logo/logo_ys.png" alt="logo">
+                            </a>
+                        </div>
+                        <nav class="menu">
+                            <ul class="mainmenu">
+                                <li class="current"><a href="index.php"><span class="flaticon-home"></span>Home</a>
+                                </li>
+                                <li class="dropdown-trigger">
+                                    <a href="#"><span class="flaticon-photo"></span>About The Party</a>
+                                    <ul class="dropdown-content">
+                                        <li><a href="our_mission.php">Our Vision</a></li>
+                                        <li><a href="history.php">Party History</a></li>
+                                        <li><a href="idiology.php">Party Ideology</a></li>
+                                        <li><a href="#">Our Leader</a></li>
+                                        <li><a href="achievements.php">Our Achievements</a></li>
+                                    </ul>
+                                </li>
+                                 <li class="dropdown-trigger">
+                                    <a href="#"><span class="flaticon-graphic-design-1"></span>Media</a>
+                                    <ul class="dropdown-content">
+                                        <li><a href="latest_news.php">Latest News</a></li>
+                                        <li><a href="video_gallery.php">Speeches</a></li>
+                                        <li><a href="photo_gallery.php">Photos</a></li>
+                                        <li><a href="video_gallery.php">Videos</a></li>
+                                        <li><a href="#">Press Releases</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="in_focus.php"><span class="flaticon-eye"></span>In Focus</a></li>
+                                <li><a href="join_us.php"><span class="flaticon-analytics"></span>Join Us</a></li>
+                                <li><a href="contact.php"><span class="flaticon-chat"></span>Contact us</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="mobile-menu hidden-md hidden-lg">
+                        <a class="mobile-logo" href="index.php"><img src="images/logo/logo_ys.png" alt="logo"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header><!-- End Site Header -->
+
+    <!-- For Sticky header -->
+    <div id="sticky-header"></div>
+
+
+    <!-- Start Page Heading area -->
+    <div class="page_heading_area ptb-110 bg-image bg_overlay bg-img-fixed" style="background-image: url(images/slider/3.jpg);">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="page_title">
+                        <h1>OUR LEADER</h1>
+                        <p>Stay connect us</p>
+                        <div class="divider">
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div><!--/.page_title-->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Page Heading area -->
+
+
+    <!-- Start page section area -->
+    <section id="page" class="page-section">
+        <!-- Start Smart Team Area -->
+        <div class="smart_team_area mtb-100">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <div class="section_title text-center">
+                            <h2 class="title__heading text-capitalize">Our Leader</h2>
+                            <p>we are most exprianceable leader</p>
+                        </div><!--/.section_title-->
+                    </div>
+                </div><!--/.row-->
+                <div class="row">
+                    <div class="col-md-12 col-sm-12">
+                        <div id="team_carousel" class="owl-carousel">
+                            <!-- Start Single Team -->
+                            <div class="single_team text-center">
+                                <figure class="team_thumb image_effect">
+                                    <img src="images/team/5.jpg" alt="thmub">
+                                </figure>
+                                <div class="team_info">
+                                    <h3>Siddhesh Kadam</h3>
+                                    <ul class="team_social">
+                                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div><!-- End Single Team -->
+                            <!-- Start Single Team -->
+                            <div class="single_team text-center">
+                                <figure class="team_thumb image_effect">
+                                    <img src="images/team/6.jpg" alt="thmub">
+                                </figure>
+                                <div class="team_info">
+                                    <h3>Shrikant Shinde</h3>
+                                    <ul class="team_social">
+                                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div><!-- End Single Team -->
+                            <!-- Start Single Team -->
+                            <div class="single_team text-center">
+                                <figure class="team_thumb image_effect">
+                                    <img src="images/team/7.jpg" alt="thmub">
+                                </figure>
+                                <div class="team_info">
+                                    <h3>Adesh Bandekar</h3>
+                                    <ul class="team_social">
+                                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div><!-- End Single Team -->
+                            <!-- Start Single Team -->
+                            <div class="single_team text-center">
+                                <figure class="team_thumb image_effect">
+                                    <img src="images/team/8.jpeg" alt="thmub">
+                                </figure>
+                                <div class="team_info">
+                                    <h3>Nitin Sardesai</h3>
+                                    <ul class="team_social">
+                                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div><!-- End Single Team -->
+                            <!-- Start Single Team -->
+                            <div class="single_team text-center">
+                                <figure class="team_thumb image_effect">
+                                    <img src="images/team/9.jpg" alt="thmub">
+                                </figure>
+                                <div class="team_info">
+                                    <h3>Sanjay Raut</h3>
+                                    <ul class="team_social">
+                                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div><!-- End Single Team -->
+                        </div>
+                    </div><!--/.col-md-12-->
+                </div>
+            </div>
+        </div>
+        <!-- End Smart Team Area -->
+
+    </section>
+    <!-- End page section area -->
+
+
+    <?php include("footer.php") ?> 
+
+     <!-- jQuery javascripts -->
+    <script src="js/vendor/jquery.min.js"></script>
+    <!-- Bootstrap javascrits -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Plugins scripts -->
+    <script src="js/plugins.js"></script>
+    <!-- Main scripts -->
+    <script src="js/main.js"></script>
+</body>
+</html>
